d42b076eaf0d750be87ab808fcf1e263d65b68da
dotcirill <dotcirill@gmail.com>
2022-05-08 22:47:46 +0300

    Add single quote string (Lexer)

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 for a, *b, in lambda X, *C, **D,: X^C@D and 7 == (lambda: lambda: ... if [...,(3 for _ in X),] is not [] else ()):
 	while qwe <= 123 <= 12 in abc:
-		if -------3+3**3**-9**-66 > 5%"abc" "qwe":
+		if -------3+3**3**-9**-66 > 5%"abc" 'qwe':
 			break
 		a = 213
 
